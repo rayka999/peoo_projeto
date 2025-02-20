69b6c41305b23a8f9d2a2c14498087838b6f08ec
rayka999 <raykalgacria07@gmail.com>
2025-02-20 10:18:22 -0300

    ...

--- src/app.ts
+++ src/app.ts
@@ -2,7 +2,7 @@ import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
 dotenv.config(); // Isso vai carregar as variáveis de ambiente do arquivo .env
-import turmaRoutes from './routes/turmaRoutes.js'
+//import turmaRoutes from './routes/turmaRoutes.js'
 
 const app = express();
 
@@ -10,7 +10,7 @@ app.use(cors()); // Permitir requisições de diferentes origens
 app.use(express.json()); // Para manipular requisições JSON
 
 // Configuração das rotas
-app.use('/api/turma', turmaRoutes);
+//app.use('/api/turma', turmaRoutes);
 
 console.log('Rotas carregadas corretamente!');
 
